734003a35e9b662b4b0d8dd06421e239f84db8ea
Soham Mukherjee <129587134+sohamfcb@users.noreply.github.com>
2025-04-08 13:48:20 +0530

    added a rudimentary agentic chatbot

---
+++ home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="SkillSync AI",
+                   layout="wide",
+                   page_icon='logo.jpg')
+
+st.write("Welcome to our Home Page")
